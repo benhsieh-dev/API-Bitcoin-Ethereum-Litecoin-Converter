c5aae4dc70e4f00864bb9b07bd036969ee8ab18d
Kevin Hsieh <bqh5026@Kevins-iMac.local>
2019-08-04 23:49:32 -0400

    Initial deployment

---
+++ README.md
@@ -0,0 +1,3 @@
+#Converts Bitcoin/Ethereum/Litecoin into USD/GBP/EUR
+
+* Makes use of [bitcoinaverage](https://apiv2.bitcoinaverage.com/) APIs 
---
+++ index.js
@@ -0,0 +1,55 @@
+//jshint esversion: 6
+
+const express    = require('express'),
+      bodyParser = require('body-parser'),
+      request    = require('request');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+// app.use('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post('/', (req, res) => {
+
+  let crypto = req.body.crypto;
+  let fiat = req.body.fiat;
+  let amount = req.body.amount;
+
+  let options = {
+    url: "https://apiv2.bitcoinaverage.com/convert/global?",
+    method: "GET",
+    qs: {
+      from: crypto,
+      to: fiat,
+      amount: amount
+    }
+  };
+
+  request(options, (error, response, body) => {
+
+    let data = JSON.parse(body);
+    let price = data.price;
+
+    let currentDate = data.time;
+
+    res.write("<p>The current date is " + currentDate + "</p>");
+    res.write("<h1>" + amount + crypto + " is " + price + " "  + fiat + "</h1>");
+
+    res.send();
+
+  });
+
+});
+
+
+
+let port = 3000 || process.env.PORT;
+
+app.listen(port, (req, res) => {
+  console.log('server is up and running');
+});
